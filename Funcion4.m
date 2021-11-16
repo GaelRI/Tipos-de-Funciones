83d9f22416a3f9d72dd4abebc246620f2a9ee81f
GaelRI <91554106+GaelRI@users.noreply.github.com>
2021-11-15 20:35:48 -0600

    Add files via upload

---
+++ Funcion4.m
@@ -0,0 +1,27 @@
+% octave.scriptFuncio1
+% Escuela: Tecnologico de Estudios Superiores de Jilotepec
+% Especialidad: Ingeniería en Sistemas Computacionales
+% Titulo:Funcion Inyectiba, Sobreyectiva y Biyectiva 
+% Descripción:Demostar Funciones 
+% Autor:Gael Franco García
+% Fecha:15-11-2021
+% Versión: 1
+% Notas:Requierepaquete symbolic
+% Funcion 2
+
+
+pkg load symbolic
+syms x
+%Dominio x 
+x=[-32:1:45];
+%Funciona  plotar 
+rx= nthroot(x.^3,3);
+plot(x,rx)
+%marcar el plano cartesiano 
+hold on
+grid on;
+plot([-40 60],[0 0],'k+-',"linewidth",2,"markersize",8);
+plot([0 0],[-60 60],'k+-',"linewidth",2,"markersize",8);
+title(['Funcion 4 Biyectiva']);
+disp('Sea f:D?I es biyectiva ? es inyectiva y suprayectiva al mismo tiempo')
+disp('POr lo tanto esta funcion es Biyectiva, por que se comporta tanto como una inyectiva y una suprayectiva')
---
+++ Funcion5.m
@@ -0,0 +1,29 @@
+% octave.scriptFuncio1
+% Escuela: Tecnologico de Estudios Superiores de Jilotepec
+% Especialidad: Ingeniería en Sistemas Computacionales
+% Titulo:Funcion Inyectiba, Sobreyectiva y Biyectiva 
+% Descripción:Demostar Funciones 
+% Autor:Gael Franco García
+% Fecha:15-11-2021
+% Versión: 1
+% Notas:Requierepaquete symbolic
+% Funcion 2
+
+
+pkg load symbolic
+syms r
+%Dominio r 
+r=[-30:1:-1];
+%Funciona  plotar 
+rx=1./(r.^3);
+plot(r,rx)
+%marcar el plano cartesiano 
+hold on
+grid on;
+plot([-30 30],[0 0],'k+-',"linewidth",2,"markersize",8);
+plot([0 0],[-1 1],'k+-',"linewidth",2,"markersize",8);
+title(['Funcion 5 No es ningun tipo de funcion  ']);
+disp('Esta Funcion/Grafica No es ningun tipo de funcion')
+disp('No cumple con  ?x,y ?D con x?y se cumple f(x)?f(y)')
+disp('No cumple con ?y ?I ? x?D | f(x)=y')
+disp('Por lo tantp tampoco puede ser Biyectiva pues no es inyectiva ni suprayectiva.);
