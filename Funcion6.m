254dd1a700a00f77474f0c99f37e9ac8017928ae
GaelRI <91554106+GaelRI@users.noreply.github.com>
2021-11-15 20:39:16 -0600

    Add files via upload

---
+++ Funcion6.m
@@ -0,0 +1,26 @@
+% octave.scriptFuncio1
+% Escuela: Tecnologico de Estudios Superiores de Jilotepec
+% Especialidad: Ingeniería en Sistemas Computacionales
+% Titulo:Funcion Inyectiba, Sobreyectiva y Biyectiva 
+% Descripción:Demostar Funciones 
+% Autor:Gael Franco García
+% Fecha:15-11-2021
+% Versión: 1
+% Notas:Requierepaquete symbolic
+% Funcion 6
+
+
+ pkg load symbolic
+syms x
+%Rango x 
+x=[-20:1:20];
+%Funciona  plotar 
+fx=((x.^4)(+1)./(x.^3));
+plot(x,fx)
+%marcar el plano cartesiano 
+hold on
+grid on;
+title(['Funcion 6 Biyectiva']);
+disp('Cumple con  ?x,y ?D con x?y se cumple f(x)?f(y)')
+disp('Cumple con ?y ?I ? x?D | f(x)=y')
+disp('Por lo tanto al compartir ambas es Biyectiva')
